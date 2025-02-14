932fd10bdda4dbbd81f60cddabb9602a5f73be6c
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-02-14 21:59:48 +0100

    refactor(OutputComponent): applicate edited things in AComponent

--- src/Components/OutputComponent.cpp
+++ src/Components/OutputComponent.cpp
@@ -9,15 +9,21 @@
 
 nts::OutputComponent::OutputComponent(std::string name) : AComponent(name, OUTPUT)
 {
+    _pins[1] = UNDEFINED;
 }
 
 nts::Tristate nts::OutputComponent::compute(std::size_t pin)
 {
-    (void)pin;
+    if (pin == 1) {
+        if (_links.find(1) != _links.end())
+            return _links[1].first->compute(_links[1].second);
+        return UNDEFINED;
+    }
     return UNDEFINED;
 }
 
 void nts::OutputComponent::simulate(std::size_t tick)
 {
     (void)tick;
+    _pins[1] = compute(1);
 }
