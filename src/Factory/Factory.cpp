9dc3c095d4fd087ae1215fe57ea7d9957a0c8c08
Rafael Drouart <rafael.drouart@epitech.eu>
2025-02-14 14:18:51 +0100

    Fix(factory): Modifying the factory to use a map instead a lot of ifs

--- src/Factory/Factory.cpp
+++ src/Factory/Factory.cpp
@@ -6,7 +6,6 @@
 */
 
 #include "Factory.hpp"
-
 #include "Components/AndComponent.hpp"
 #include "Components/ClockComponent.hpp"
 #include "Components/InputComponent.hpp"
@@ -17,16 +16,26 @@
 #include "Components/OutputComponent.hpp"
 #include "Components/XorComponent.hpp"
 
+Factory::Factory()
+{
+    componentMap = {
+            {"and", []() { return std::make_unique<nts::AndComponent>("and"); }},
+            {"clock", []() { return std::make_unique<nts::ClockComponent>("clock"); }},
+            {"input", []() { return std::make_unique<nts::InputComponent>("input"); }},
+            {"nand", []() { return std::make_unique<nts::NandComponent>("nand"); }},
+            {"nor", []() { return std::make_unique<nts::NorComponent>("nor"); }},
+            {"not", []() { return std::make_unique<nts::NotComponent>("not"); }},
+            {"or", []() { return std::make_unique<nts::OrComponent>("or"); }},
+            {"output", []() { return std::make_unique<nts::OutputComponent>("output"); }},
+            {"xor", []() { return std::make_unique<nts::XorComponent>("xor"); }}
+    };
+}
+
 std::unique_ptr<nts::IComponent> Factory::createComponent(const std::string &type)
 {
-    if (type == "and") return std::make_unique<nts::AndComponent>("and");
-    if (type == "clock") return std::make_unique<nts::ClockComponent>("clock");
-    if (type == "input") return std::make_unique<nts::InputComponent>("input");
-    if (type == "nand") return std::make_unique<nts::NandComponent>("nand");
-    if (type == "nor") return std::make_unique<nts::NorComponent>("nor");
-    if (type == "not") return std::make_unique<nts::NotComponent>("not");
-    if (type == "or") return std::make_unique<nts::OrComponent>("or");
-    if (type == "output") return std::make_unique<nts::OutputComponent>("output");
-    if (type == "xor") return std::make_unique<nts::XorComponent>("xor");
+    auto it = componentMap.find(type);
+    if (it != componentMap.end()) {
+        return it->second();
+    }
     return nullptr;
 }
--- src/Factory/Factory.hpp
+++ src/Factory/Factory.hpp
@@ -9,14 +9,18 @@
 #define FACTORY_HPP
 
 #include <memory>
-
+#include <map>
+#include <functional>
 #include "NanoTekSpice.hpp"
 
 class Factory {
 public:
-    Factory() = default;
+    Factory();
     ~Factory() = default;
      std::unique_ptr<nts::IComponent> createComponent(const std::string &type);
+
+private:
+    std::map<std::string, std::function<std::unique_ptr<nts::IComponent>()>> componentMap;
 };
 
 
