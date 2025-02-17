33bb0fbc72c1704e42361a355095a0883bea82b2
Rafael Drouart <rafael.drouart@epitech.eu>
2025-02-17 21:23:45 +0100

    feat(elementaryComponents): Adding the xor component

---
+++ src/Components/XorComponent.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** nanotekspice
+** File description:
+** XorComponent.cpp
+*/
+
+#include "XorComponent.hpp"
+
+nts::XorComponent::XorComponent(std::string name) : AComponent(name, XOR)
+{
+    _pins[1] = UNDEFINED;
+    _pins[2] = UNDEFINED;
+    _pins[3] = UNDEFINED;
+}
+
+nts::Tristate nts::XorComponent::compute(std::size_t pin)
+{
+    if (pin == 3) {
+        Tristate a = getLink(1);
+        Tristate b = getLink(2);
+
+        if ((a == TRUE && b == FALSE) || (a == FALSE && b == TRUE))
+            return TRUE;
+        if ((a == TRUE && b == TRUE) || (a == FALSE && b == FALSE))
+            return FALSE;
+        return UNDEFINED;
+    }
+    return UNDEFINED;
+}
+
+void nts::XorComponent::simulate(std::size_t tick)
+{
+    (void)tick;
+}
